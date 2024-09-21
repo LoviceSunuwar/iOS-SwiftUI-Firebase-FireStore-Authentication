95f909d6a054bf5420ae6b203b18326b3db77392
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-09-21 18:22:32 -0400

    Created a landing view for the signin page, NaivgationView Used

---
+++ FireCRUD/Authentication/SignInEmailView.swift
@@ -0,0 +1,22 @@
+//
+//  SignInEmailView.swift
+//  FireCRUD
+//
+//  Created by Lovice Sunuwar on 21/09/2024.
+//
+
+import SwiftUI
+
+final class SignInEmailViewModel: ObservableObject {
+    
+}
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInEmailView()
+}
