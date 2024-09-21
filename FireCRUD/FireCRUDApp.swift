7980720a377a7fd1e5d548b61c093f04298ebc11
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-09-21 18:10:49 -0400

    Base Project, Added dependencies with SPM, COnnected to firebase

--- FireCRUD/FireCRUDApp.swift
+++ FireCRUD/FireCRUDApp.swift
@@ -4,11 +4,25 @@
 //
 //  Created by Lovice Sunuwar on 21/09/2024.
 //
-
 import SwiftUI
+import FirebaseCore
+
+// MARK: AppDelegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct FireCRUDApp: App {
+//    MARK: Firebase Init
+    // Initializng the firebase via appDeleate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
