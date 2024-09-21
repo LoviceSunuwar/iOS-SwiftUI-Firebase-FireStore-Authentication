c31a859d0dbf02ab23c3608e43ec03791a42c18b
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-09-21 17:50:09 -0400

    Intiating an iOS APp, that utilizies Firebase functionalities.

---
+++ FireCRUD/FireCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireCRUDApp.swift
+//  FireCRUD
+//
+//  Created by Lovice Sunuwar on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FireCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# iOS-SwiftUI-Firebase-FireStore-Authentication
